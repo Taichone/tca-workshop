d01423672daf13190fc42cd2f9a38cee05b7d7e0
Taichone <taichone.dev@gmail.com>
2024-05-11 22:04:33 +0900

    Sec2: SwiftUI View の実装

--- Sources/RepositoryListFeature/RepositoryListView.swift
+++ Sources/RepositoryListFeature/RepositoryListView.swift
@@ -1,10 +1,78 @@
 import Composable ComposableArchitecture
 import Entity
 import Foundation
+import SwiftUI
+
+// View と Reducer は同じファイルにしておくとすぐに互いの実装を確認できて便利
+// Point-Free 公式も同じファイルにするのが好みらしい
+
+
+public struct RepositoryListView: View {
+    // Reducer と View を繋ぐ Store API
+    // 機能におけるランタイムとしての責務
+    // - Reducer の実装に従って State を更新するために Action を処理したり、Effect を実行してくれたりする
+    let store: StoreOf<RepositoryList> // StoreOf<X> は Store<X.State, X.Action> の typealias
+    
+    public init(store: StoreOf<RepositoryList>) {
+        self.store = store
+    }
+    
+    public var body: some View {
+        Group {
+            // store.someState の形で State を取得できる
+            if store.isLoading {
+                ProgressView()
+            } else { // isLoading が false になった == API Response が帰ってきた場合の View を実装
+                List {
+                  ForEach(store.repositories, id: \.id) { repository in
+                    Button {
+                        // TODO: 後ほど実装
+                    } label: {
+                      VStack(alignment: .leading, spacing: 8) {
+                        Text(repository.fullName)
+                          .font(.title2.bold())
+                        Text(repository.description ?? "")
+                          .font(.body)
+                          .lineLimit(2)
+                        HStack(alignment: .center, spacing: 32) {
+                          Label(
+                            title: {
+                              Text("\(repository.stargazersCount)")
+                                .font(.callout)
+                            },
+                            icon: {
+                              Image(systemName: "star.fill")
+                                .foregroundStyle(.yellow)
+                            }
+                          )
+                          Label(
+                            title: {
+                              Text(repository.language ?? "")
+                                .font(.callout)
+                            },
+                            icon: {
+                              Image(systemName: "text.word.spacing")
+                                .foregroundStyle(.gray)
+                            }
+                          )
+                        }
+                      }
+                      .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                    .buttonStyle(.plain)
+                  }
+                }
+            }
+        }
+        .onAppear {
+            // store.send(.someAction) という形で Action を送ることができる
+            self.store.send(.onAppear)
+        }
+    }
+}
 
 // TCA に用意されている Reducer protocol に準拠した構造体
 // 準拠させる書き方ではなく @Reducer (Swift Macros) を使う
-
 @Reducer
 public struct RepositoryList {
     // MARK: - @Reducer 準拠には State が必要
@@ -31,8 +99,7 @@ public struct RepositoryList {
     // MARK: - @Reducer 準拠には body が必要
     // - 何らかの Action が与えられた時に State を現在の値から次の値へと変更する責務
     // - アプリが外の世界で実行すべき処理である Effect を return する責務（API 通信や UserDefaults へのアクセスなどが該当）
-    // ReducerOf<Self> は Reducer<Self.State, Self.Action> の typealias（TCA では XOf<Y> のような typealias がいくつか出てくる）
-    public var body: some ReducerOf<Self> {
+    public var body: some ReducerOf<Self> { // ReducerOf<Self> は Reducer<Self.State, Self.Action> の typealias
         // TCA が用意している Reduce API を用いれば上記の責務を表現できる
         Reduce { state, action in
             switch action {
@@ -42,7 +109,7 @@ public struct RepositoryList {
                 
                 // 基本的に TCA では Reducer において Effect.run の中でのみ 非同期処理を実行できる
                 return .run { send in // Effect.run の closure には Send 型（ @MainActor 付き）が提供されている
-                    // await send(.some Action) のような形でに2の Action を発火できる
+                    // await send(.someAction) のような形で Action を発火できる
                     await send(
                         .searchRepositoriesResponse(
                             // GitHub API の search エンドポイントに対してリクエスト
